a4ed38ef148874028c7491c1988061c22d3ae018
Pranshmish <pragya636@gmail.com>
2025-09-23 21:24:39 +0530

    React and tailwind Set Up

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-blue-500 text-white p-4  text-center'>
+        <p>Hello World</p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
